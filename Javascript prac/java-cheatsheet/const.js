bf86fe025d2f7977265472f7c64fb0821964836f
Kycodes123 <100347576+Kycodes123@users.noreply.github.com>
2022-08-11 08:05:05 +0200

    Add files via upload

---
+++ Javascript prac/java-cheatsheet/const.js
@@ -0,0 +1,14 @@
+// const = a variable that can't be changed 
+// Adds security in your code 
+
+
+const PI = 3.14159; //the variable can't be changed * consts are usually all uppercase  PI, STUDENTS, etc
+let radius;
+let circumference;
+
+radius = window.prompt("Enter circle radius");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+
+console.log("The cicumference is:", circumference);
